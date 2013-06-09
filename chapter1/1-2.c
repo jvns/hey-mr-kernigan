22271db4965f28dbe3afd9b09585e31b563428e6
Julia Evans <julia@jvns.ca>
2013-06-08 21:16:15 -0400

    exercise 1-2: print hello world using backspaces and carriage returns and things

---
+++ chapter1/1-2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// Prints "hello, world"
+int main()
+{
+  printf("asdlo, worjd");
+  // Go back!
+  printf("\b\b");
+  printf("l");
+  // Carriage return!
+  printf("\r");
+  printf("hel");
+  printf("\n");
+}
